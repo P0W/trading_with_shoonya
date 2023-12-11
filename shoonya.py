98bcfff705de29aefc60c5dd9c59bd5724774df4
Prashant Srivastava <prashants@microsoft.com>
2023-12-11 13:35:59 +0530

    updates for target pnl and close pending sl orders

--- shoonya.py
+++ shoonya.py
@@ -9,6 +9,7 @@ import os
 import pathlib
 import sys
 import time
+import traceback
 import zipfile
 
 import pandas as pd
@@ -17,8 +18,21 @@ import redis
 import requests
 import yaml
 
-from api_helper import Order
-from api_helper import ShoonyaApiPy
+
+from NorenRestApiPy.NorenApi import NorenApi
+
+
+class ShoonyaApiPy(NorenApi):
+    """
+    Shoonya API Initializer
+    """
+
+    def __init__(self):
+        NorenApi.__init__(
+            self,
+            host="https://api.shoonya.com/NorenWClientTP/",
+            websocket="wss://api.shoonya.com/NorenWSTP/",
+        )
 
 
 def configure_logger(log_level, prefix_log_file: str = "shoonya_daily_short"):
@@ -45,8 +59,6 @@ def configure_logger(log_level, prefix_log_file: str = "shoonya_daily_short"):
     )
 
 
-api = ShoonyaApiPy()
-
 INDICES_TOKEN = {
     "NIFTY": "26000",
     "BANKNIFTY": "26009",
@@ -58,6 +70,13 @@ INDICES_ROUNDING = {
     "NIFTY": 50,
     "BANKNIFTY": 100,
     "FINNIFTY": 50,
+}
+
+LOT_SIZE = {
+    "NIFTY": 50,
+    "BANKNIFTY": 15,
+    "FINNIFTY": 40,
+    "SENSEX": 10,
 }
 
 
@@ -93,7 +112,7 @@ def download_scrip_master():
     return df
 
 
-def get_staddle_strike(symbol_index):
+def get_staddle_strike(shoonya_api, symbol_index):
     """
     Get the nearest strike for the index
     """
@@ -109,7 +128,7 @@ def get_staddle_strike(symbol_index):
     expiry_date = df.iloc[0]["Expiry"]
     ## convert to 06DEC23
     expiry_date = expiry_date.strftime("%d%b%y").upper()
-    ret = api.get_quotes(exchange="NSE", token=INDICES_TOKEN[symbol_index])
+    ret = shoonya_api.get_quotes(exchange="NSE", token=INDICES_TOKEN[symbol_index])
     if ret:
         ltp = float(ret["lp"])
         ## round to nearest INDICES_ROUNDING
@@ -121,8 +140,8 @@ def get_staddle_strike(symbol_index):
         ## find the token for the strike
         ce_token = df[df["TradingSymbol"] == ce_strike]["Token"].values[0]
         pe_token = df[df["TradingSymbol"] == pe_strike]["Token"].values[0]
-        ce_quotes = api.get_quotes(exchange="NFO", token=str(ce_token))
-        pe_quotes = api.get_quotes(exchange="NFO", token=str(pe_token))
+        ce_quotes = shoonya_api.get_quotes(exchange="NFO", token=str(ce_token))
+        pe_quotes = shoonya_api.get_quotes(exchange="NFO", token=str(pe_token))
         return {
             "ce_code": str(ce_token),
             "pe_code": str(pe_token),
@@ -141,7 +160,7 @@ def round_to_point5(x):
     return round(x * 2) / 2
 
 
-def login(force=False):
+def login(shoonya_api, force=False):
     """
     Login to the Shoonya API
     """
@@ -153,7 +172,7 @@ def login(force=False):
             access_token = access_token.decode("utf-8")
             with open("cred.yml", encoding="utf-8") as f:
                 cred = yaml.load(f, Loader=yaml.FullLoader)
-                api.set_session(cred["user"], cred["pwd"], access_token)
+                shoonya_api.set_session(cred["user"], cred["pwd"], access_token)
             logging.info("Access token found in cache, logging in")
         else:
             raise ValueError("No access token found")
@@ -162,7 +181,7 @@ def login(force=False):
         with open("cred.yml", encoding="utf-8") as f:
             cred = yaml.load(f, Loader=yaml.FullLoader)
 
-            ret = api.login(
+            ret = shoonya_api.login(
                 userid=cred["user"],
                 password=cred["pwd"],
                 twoFA=pyotp.TOTP(cred["totp_pin"]).now(),
@@ -179,56 +198,48 @@ def login(force=False):
                 pass
 
 
-def place_straddle(strikes_data, qty=15):
+def place_straddle(shoonya_api, strikes_data, qty):
     """
     Place a straddle order for the index
     """
     if not strikes_data:
-        return
-    logging.info("Strikes: %s", json.dumps(strikes_data, indent=2))
-    straddle_orders = [
-        {
-            "buy_or_sell": "S",
-            "product_type": "M",
-            "exchange": "NFO",
-            "tradingsymbol": strikes_data["ce_strike"],
-            "quantity": qty,
-            "discloseqty": 0,
-            "price_type": "LMT",
-            "price": strikes_data["ce_ltp"],
-            "trigger_price": None,
-            "retention": "DAY",
-            "remarks": "ce_straddle",
-        },
-        {
-            "buy_or_sell": "S",
-            "product_type": "M",
-            "exchange": "NFO",
-            "tradingsymbol": strikes_data["pe_strike"],
-            "quantity": qty,
-            "discloseqty": 0,
-            "price_type": "LMT",
-            "price": strikes_data["pe_ltp"],
-            "trigger_price": None,
-            "retention": "DAY",
-            "remarks": "pe_straddle",
-        },
-    ]
+        return None
     ## all_orders is of type Order
-    straddle_orders_obj = [Order(**order) for order in straddle_orders]
-    logging.info("Placing straddle: %s", json.dumps(straddle_orders, indent=2))
-    response = api.place_basket(straddle_orders_obj)
-    logging.info("Response: %s", json.dumps(response, indent=2))
+    placed_orders = []
+    for item in ["ce", "pe"]:
+        logging.info("Placing order for %s", item)
+        response = shoonya_api.place_order(
+            buy_or_sell="S",
+            product_type="M",
+            exchange="NFO",
+            tradingsymbol=strikes_data[f"{item}_strike"],
+            quantity=qty,
+            discloseqty=0,
+            price_type="LMT",
+            price=strikes_data[f"{item}_ltp"],
+            trigger_price=None,
+            retention="DAY",
+            remarks=f"{item}_straddle",
+        )
+        if response["stat"] == "Ok":
+            logging.info("Order placed for %s", item)
+            logging.info("Response: %s", json.dumps(response, indent=2))
+            placed_orders.append(response["norenordno"])
+        else:
+            logging.error("Could not place order for %s", item)
+            logging.error("Response: %s", json.dumps(response, indent=2))
+            sys.exit(-1)
+    return placed_orders
 
-
-def place_sl_order(tsym, qty, lp, remarks, sl_factor):
+## pylint: disable=too-many-arguments
+def place_sl_order(shoonya_api, tsym, qty, lp, remarks, sl_factor):
     """
     Place a stop loss order
     """
     lp = float(lp)
     sl = round_to_point5(lp * sl_factor)
     trigger = sl - 0.5
-    ret = api.place_order(
+    response = shoonya_api.place_order(
         buy_or_sell="B",
         product_type="M",
         exchange="NFO",
@@ -241,54 +252,94 @@ def place_sl_order(tsym, qty, lp, remarks, sl_factor):
         retention="DAY",
         remarks=f"{remarks}_stop_loss",
     )
-    logging.info("Placed stop loss order: %s", json.dumps(ret, indent=2))
+    logging.info("Placed stop loss order: %s", json.dumps(response, indent=2))
+    if response["stat"] == "Ok":
+        return response["norenordno"]
+    return None
 
 
-def pnl_monitor(pnl, target):
+def pnl_monitor(shoonya_api, pnl, existing_orders, target):
     """
     Monitor pnl
     """
     continue_running = True
     if pnl > target:
-        logging.info("PNL > %.2f, exiting", target)
-        ret = api.get_order_book()
+        logging.info("Target Achieved | PNL > %.2f | exiting", target)
+        logging.info("Existing orders %s", json.dumps(existing_orders, indent=2))
+        ret = shoonya_api.get_order_book()
         for order in ret:
             if order["status"] != "COMPLETE":
                 continue
-            if order["remarks"] == "pe_straddle" or order["remarks"] == "ce_straddle":
-                logging.info("Unsubscribing from %s", order["tsym"])
-                api.unsubscribe(f"NFO|{order['tsym']}")
-                logging.info("Exiting Leg")
-                response = api.place_order(
-                    buy_or_sell="B",
-                    product_type="M",
-                    exchange="NFO",
-                    tradingsymbol=order["tsym"],
-                    quantity=order["qty"],
-                    discloseqty=0,
-                    price_type="MKT",
-                    price=0,  ## market order
-                    trigger_price=None,
-                    retention="DAY",
-                    remarks=f"{order['remarks']}_exit",
-                )
-                logging.info(
-                    "Response exit position %s", json.dumps(response, indent=2)
-                )
-        ret = api.get_order_book()
-        for order in ret:
-            if order["status"] != "TRIGGER_PENDING":
-                continue
-            if (
-                order["remarks"] == "pe_strangle_stop_loss"
-                or order["remarks"] == "ce_strangle_stop_loss"
+            if ("remarks" in order) and (
+                order["remarks"] == "pe_straddle" or order["remarks"] == "ce_straddle"
             ):
                 norenordno = order["norenordno"]
-                logging.info("Cancelling pending stop loss orders")
-                response = api.cancel_order(norenordno)
-                logging.info("Response cancel order %s", json.dumps(response, indent=2))
+                if norenordno in existing_orders:
+                    logging.info("Unsubscribing from %s", order["tsym"])
+                    shoonya_api.unsubscribe(f"NFO|{order['tsym']}")
+                    logging.info("Exiting Leg")
+                    response = shoonya_api.place_order(
+                        buy_or_sell="B",
+                        product_type="M",
+                        exchange="NFO",
+                        tradingsymbol=order["tsym"],
+                        quantity=order["qty"],
+                        discloseqty=0,
+                        price_type="MKT",
+                        price=0,  ## market order
+                        trigger_price=None,
+                        retention="DAY",
+                        remarks=f"{order['remarks']}_exit",
+                    )
+                    logging.info(
+                        "Response exit position %s", json.dumps(response, indent=2)
+                    )
+        ret = shoonya_api.get_order_book()
+        for order in ret:
+            if order["status"] == "TRIGGER_PENDING" and ("remarks" in order):
+                if (
+                    order["remarks"] == "pe_straddle_stop_loss"
+                    or order["remarks"] == "ce_straddle_stop_loss"
+                ):
+                    norenordno = order["norenordno"]
+                    if norenordno in existing_orders:
+                        logging.info("Cancelling pending stop loss orders")
+                        response = shoonya_api.cancel_order(norenordno)
+                        logging.info(
+                            "Response cancel order %s", json.dumps(response, indent=2)
+                        )
         continue_running = False
+    else:
+        logging.info("PNL %.2f | Target %.2f", pnl, target)
     return continue_running
+
+
+def full_stack():
+    """
+    Get the full stack trace
+    """
+    exc = sys.exc_info()[0]
+    stack = traceback.extract_stack()[:-1]  # last one would be full_stack()
+    if exc is not None:  # i.e. an exception is present
+        del stack[-1]  # remove call of full_stack, the printed exception
+        # will contain the caught exception caller instead
+    trc = "Traceback (most recent call last):\n"
+    stackstr = trc + "".join(traceback.format_list(stack))
+    if exc is not None:
+        stackstr += "  " + traceback.format_exc()
+    return stackstr
+
+
+def validate(index_qty, index_value):
+    """
+    Validate the quantity
+    """
+    if index_value not in INDICES_TOKEN:
+        logging.error("Invalid index %s", index_value)
+        sys.exit(-1)
+    if index_qty % LOT_SIZE[index_value] != 0:
+        logging.error("Quantity must be multiple of %s", LOT_SIZE[index_value])
+        sys.exit(-1)
 
 
 ## pylint: disable=too-many-instance-attributes
@@ -312,39 +363,45 @@ class LiveFeedManager:
         self.running = False
         self.placed_ord_callback = placed_ord_callback
         self.in_position = False
+        self.existing_orders = []
 
     def event_handler_feed_update(self, tick_data):
         """
         Event handler for feed update
         """
-        if not self.strikes or not self.running:
-            unsusbscribe_symbols = [
-                f"NFO|{self.strikes['ce_code']}",
-                f"NFO|{self.strikes['pe_code']}",
-            ]
-            self.api.unsubscribe(unsusbscribe_symbols)
-            self.api.close_websocket()
-            self.in_position = False
-            return
-        msg = []
-        if "lp" in tick_data and self.in_position:
-            if tick_data["tk"] == self.strikes["ce_code"]:
-                self.pnl[tick_data["tk"]] = self.strikes["ce_ltp"] - float(
-                    tick_data["lp"]
+        try:
+            if not self.strikes or not self.running:
+                unsusbscribe_symbols = [
+                    f"NFO|{self.strikes['ce_code']}",
+                    f"NFO|{self.strikes['pe_code']}",
+                ]
+                self.api.unsubscribe(unsusbscribe_symbols)
+                self.in_position = False
+                return
+            msg = []
+            if "lp" in tick_data and self.in_position:
+                if tick_data["tk"] == self.strikes["ce_code"]:
+                    self.pnl[tick_data["tk"]] = self.strikes["ce_ltp"] - float(
+                        tick_data["lp"]
+                    )
+                    msg.append(f"CE lp: {float(tick_data['lp'])}")
+                elif tick_data["tk"] == self.strikes["pe_code"]:
+                    self.pnl[tick_data["tk"]] = self.strikes["pe_ltp"] - float(
+                        tick_data["lp"]
+                    )
+                    msg.append(f"PE lp: {float(tick_data['lp'])}")
+            if len(self.pnl) == 2:
+                total_pnl = (
+                    self.pnl[self.strikes["ce_code"]]
+                    + self.pnl[self.strikes["pe_code"]]
                 )
-                msg.append(f"CE lp: {float(tick_data['lp'])}")
-            elif tick_data["tk"] == self.strikes["pe_code"]:
-                self.pnl[tick_data["tk"]] = self.strikes["pe_ltp"] - float(
-                    tick_data["lp"]
-                )
-                msg.append(f"PE lp: {float(tick_data['lp'])}")
-        if len(self.pnl) == 2:
-            total_pnl = (
-                self.pnl[self.strikes["ce_code"]] + self.pnl[self.strikes["pe_code"]]
-            )
-            msg.append(f"Total PNL: {total_pnl}")
-            logging.info("Feed Data: %s", "| ".join(msg))
-            self.running = self.monitor_function(total_pnl)
+                logging.info("Feed Data: %s", "| ".join(msg))
+                self.running = self.monitor_function(total_pnl, self.existing_orders)
+        except Exception as ex:  ## pylint: disable=broad-except
+            logging.error("Exception in feed update: %s", ex)
+            ## stacktrace
+            logging.error(full_stack())
+            sys.exit(-1)
 
     def open_callback(self):
         """
@@ -369,10 +426,12 @@ class LiveFeedManager:
             ):
                 logging.info("Straddle Placed %s", order_data["remarks"])
                 qty = order_data["qty"]
-                lp = order_data["lp"]
+                lp = order_data["prc"]
                 tsym = order_data["tsym"]
                 remarks = order_data["remarks"]
-                self.placed_ord_callback(tsym, qty, lp, remarks)
+                orderno = self.placed_ord_callback(tsym, qty, lp, remarks)
+                if orderno:
+                    self.existing_orders.append(orderno)
                 self.in_position = True
         logging.info("order update %s", json.dumps(order_data, indent=2))
 
@@ -399,6 +458,25 @@ class LiveFeedManager:
             time.sleep(0.5)
         self.running = True
 
+    def update_orders(self, all_orders):
+        """
+        Update the orders
+        """
+        for order in all_orders:
+            self.existing_orders.append(order)
+
+    def is_running(self):
+        """
+        Is running
+        """
+        return self.running
+
+    def stop(self):
+        """
+        Stop the websocket
+        """
+        self.api.stop_websocket()
+
 
 args = argparse.ArgumentParser()
 args.add_argument("--force", action="store_true", default=False)
@@ -413,14 +491,26 @@ args = args.parse_args()
 
 if __name__ == "__main__":
     configure_logger(args.log_level)
-    login(args.force)
     # subscribe to multiple tokens
     index = args.index
     quantity = args.qty
-    strikes = get_staddle_strike(index)
-    logging.info("Strikes: %s", json.dumps(strikes, indent=2))
-    target_pnl = (strikes["ce_ltp"] + strikes["pe_ltp"]) * (args.target)
-    logging.info("Target PNL: %.2f", quantity * target_pnl)
+    ## validate the quantity
+    validate(quantity, index)
+    api = ShoonyaApiPy()
+    login(api, args.force)
+    strikes = get_staddle_strike(api, index)
+    premium_collected = strikes["ce_ltp"] + strikes["pe_ltp"]
+    target_pnl = premium_collected * (args.target)
+    stop_loss = premium_collected * (args.sl_factor - 1)
+    logging.info(
+        "STARTING ALGO TRADING WITH SHOONYA on %s | Straddle Strikes %s"
+        "| Total Premium %.2f | Target %.2f | Stop Loss %.2f",
+        index,
+        json.dumps(strikes, indent=2),
+        premium_collected * quantity,
+        target_pnl * quantity,
+        stop_loss * quantity,
+    )
     if args.show_strikes:
         sys.exit(0)
     symbols = [f"NFO|{strikes['ce_code']}", f"NFO|{strikes['pe_code']}"]
@@ -429,16 +519,23 @@ if __name__ == "__main__":
         api,
         strikes,
         lambda tsyb, qty, lp, remark: place_sl_order(
-            tsyb, qty, lp, remark, args.sl_factor
+            api, tsyb, qty, lp, remark, args.sl_factor
         ),
     )
     live_feed_manager.start(
-        lambda pnl: pnl_monitor(quantity * pnl, quantity * target_pnl)
+        lambda pnl, stop_loss_orders: pnl_monitor(
+            api, quantity * pnl, stop_loss_orders, quantity * target_pnl
+        )
     )
     logging.info("Subscribing to %s", symbols)
     live_feed_manager.subscribe(symbols)
     logging.info("Waiting for 2 seconds")
     time.sleep(2)
-    place_straddle(strikes, args.qty)
-    while True:
+    orders = place_straddle(api, strikes, args.qty)
+    live_feed_manager.update_orders(orders)
+    while live_feed_manager.is_running():
         pass
+    live_feed_manager.stop()
+    logging.info("Exiting")
+    time.sleep(2)
+    logging.info("Good Bye!")
