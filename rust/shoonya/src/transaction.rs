8ab925b5ff1ae7e5ce77016cc4ade350c5d0c155
Prashant Srivastava <prashants@microsoft.com>
2024-04-01 21:06:06 +0530

    Added redis test

--- rust/shoonya/src/transaction.rs
+++ rust/shoonya/src/transaction.rs
@@ -7,10 +7,10 @@ pub mod transaction {
         resp_array,
     };
     use serde_json;
-    use std::{cell::RefCell, collections::HashMap};
+    use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
 
     pub struct TransactionManager {
-        pub redis_conn: RefCell<PairedConnection>,
+        pub redis_conn: Arc<PairedConnection>,
         pub instance: String,
     }
 
@@ -31,7 +31,7 @@ pub mod transaction {
             let instance = std::process::id().to_string();
             let utc_timestamp = chrono::Utc::now().timestamp_millis();
             Ok(TransactionManager {
-                redis_conn: RefCell::new(redis_client),
+                redis_conn: Arc::new(redis_client),
                 instance: format!("shoonya_{}_{}", instance, utc_timestamp),
             })
         }
@@ -70,10 +70,14 @@ pub mod transaction {
 
         // set the value in redis
         async fn set_value(&mut self, value: RespValue) -> bool {
+            debug!("set_value: {:?}", value);
             let redis_conn = self.redis_conn.borrow_mut().clone();
             let response: Result<String, _> = redis_conn.send(value).await;
             match response {
-                Ok(_) => true,
+                Ok(response) => {
+                    debug!("response: {:?}", response);
+                    true
+                }
                 Err(e) => {
                     error!("Failed to set value: {}", e);
                     false
@@ -105,6 +109,7 @@ pub mod transaction {
             // Make an entry in the redis db for the order
             // use redis json to store the order
             let cache_key = self.get_cache_key(&[norenordno, "order_tbl"]);
+            debug!("on_order cache_key: {:?}", cache_key);
 
             let data = resp_array![
                 "HSET",
@@ -126,15 +131,11 @@ pub mod transaction {
                 "status",
                 status,
                 "instance",
-                &self.instance,
-                "symbolcode",
-                "",
-                "ltp",
-                ""
+                &self.instance
             ];
             let _response: bool = self.set_value(data).await;
 
-            let cache_key = self.get_cache_key(&[tradingsymbol, "tradingsymbol_order_tbl"]);
+            let cache_key = self.get_cache_key(&[tradingsymbol, "tradingsymbol_tbl"]);
             let _reponse: bool = self
                 .set_value(resp_array!["SET", cache_key, norenordno])
                 .await;
@@ -143,25 +144,30 @@ pub mod transaction {
         async fn on_placed(&mut self, data: &serde_json::Value) {
             let remarks = data["remarks"].as_str().unwrap();
             if !self.validate_self(remarks.to_string()) {
+                debug!("Invalid remark {}", remarks);
                 return;
             }
             let symbolcode = data["symbolcode"].as_str().unwrap();
+            debug!("symbolcode: {:?}", symbolcode);
             let tradingsymbol = data["tradingsymbol"].as_str().unwrap();
 
-            // get the norenordno from the tradingsymbol_order_tbl
-            let cache_key = self.get_cache_key(&[tradingsymbol, "tradingsymbol_order_tbl"]);
+            // get the norenordno from the tradingsymbol_tbl
+            let cache_key = self.get_cache_key(&[tradingsymbol, "tradingsymbol_tbl"]);
 
             let norenordno: String = self.get_value(cache_key.as_str()).await;
             debug!("norenordno: {:?}", norenordno);
             // use the norenordno to get the order from order_tbl and update the symbolcode
             let cache_key = self.get_cache_key(&[norenordno.as_str(), "order_tbl"]);
+            debug!("cache_key: {:?}", cache_key);
 
             // update hset in redis with symbolcode
-            let data = resp_array!["HSET", cache_key, "symbolcode", symbolcode];
-            let _success: bool = self.set_value(data).await;
+            let data = resp_array!["HSET", cache_key, "symbolcode", &symbolcode.to_string()];
+            debug!("on_placed data: {:?}", data);
+            let success: bool = self.set_value(data).await;
+            assert!(success, "Failed to set symbolcode");
 
             // store a mapping of symbolcode to tradingsymbol
-            let cache_key = self.get_cache_key(&[symbolcode, "symb_tbl"]);
+            let cache_key = self.get_cache_key(&[&symbolcode.to_string(), "symb_tbl"]);
             let _response: bool = self
                 .set_value(resp_array!["SET", cache_key, tradingsymbol])
                 .await;
@@ -181,14 +187,14 @@ pub mod transaction {
                 let cache_key = self.get_cache_key(&[symbolcode, "symb_tbl"]);
                 debug!("cache_key: {:?}", cache_key);
                 let tradingsymbol: String = self.get_value(cache_key.as_str()).await;
-                // get the norenordno from the tradingsymbol_order_tbl
-                let cache_key =
-                    self.get_cache_key(&[tradingsymbol.as_str(), "tradingsymbol_order_tbl"]);
+                // get the norenordno from the tradingsymbol_tbl
+                let cache_key = self.get_cache_key(&[tradingsymbol.as_str(), "tradingsymbol_tbl"]);
                 debug!("cache_key: {:?}", cache_key);
                 let norenordno: String = self.get_value(cache_key.as_str()).await;
                 debug!("norenordno: {:?}", norenordno);
                 // use the norenordno to get the order from order_tbl and update the ltp
                 let cache_key = self.get_cache_key(&[norenordno.as_str(), "order_tbl"]);
+                debug!("on_tick cache_key: {:?}", cache_key);
                 let data = resp_array!["HSET", cache_key, "ltp", &lp.to_string()];
                 let _response: bool = self.set_value(data).await;
             } else {
@@ -214,8 +220,10 @@ pub mod transaction {
                 .unwrap();
             debug!("keys: {:?}", keys);
             for key in keys {
+                debug!("key: {:?}", key);
                 let order: HashMap<String, String> =
                     redis_conn.send(resp_array!["HGETALL", key]).await.unwrap();
+                debug!("order: {:?}", order);
                 let avgprice: f64 = order.get("avgprice").unwrap().parse().unwrap();
                 let qty: i64 = order.get("qty").unwrap().parse().unwrap();
                 let ltp: f64 = order.get("ltp").unwrap().parse().unwrap();
@@ -225,12 +233,78 @@ pub mod transaction {
                 if status == "COMPLETE" && avgprice != -1.0 && qty != -1 && ltp != -1.0 {
                     pnl += (ltp - avgprice) * qty as f64;
                     // pnl string as buysell tradingsymbol x qty : pnl
-                    let pnl_str = format!("{} {} x {} : {}", buysell, tradingsymbol, qty, pnl);
+                    let pnl_str = format!("{} {} x {} : {:.2}", buysell, tradingsymbol, qty, pnl);
                     pnl_vec.push(pnl_str);
                 }
             }
             let pnl_str = pnl_vec.join("");
             (pnl, pnl_str)
         }
+    }
+}
+
+// write the tests here
+#[cfg(test)]
+mod tests {
+    use super::transaction::{Transaction, TransactionManager};
+    use serde_json::json;
+
+    #[tokio::test]
+    async fn test_on_order() {
+        let mut tm = TransactionManager::new().await.unwrap();
+        let pid = std::process::id();
+        let utc_timestamp = chrono::Utc::now().timestamp_millis();
+        let instance = format!("shoonya_{}_{}", pid, utc_timestamp);
+        let remarks = format!("{}_straddle", instance);
+        let data = json!({
+            "norenordno": "123",
+            "fillshares": "100",
+            "flprc": "100.0",
+            "remarks": "straddle",
+            "trantype": "BUY",
+            "tsym": "INFY",
+            "status": "COMPLETE"
+        });
+        tm.on_order(&data).await;
+
+        let data = json!({
+            "remarks": remarks,
+            "symbolcode": "618",
+            "tradingsymbol": "INFY"
+        });
+        tm.on_placed(&data).await;
+
+        let data = json!({
+            "lp": "120.0",
+            "tk": "618"
+        });
+        tm.on_tick(&data).await;
+
+        let data = json!({
+            "norenordno": "123",
+            "fillshares": "100",
+            "flprc": "100.0",
+            "remarks": remarks,
+            "trantype": "BUY",
+            "tsym": "INFY",
+            "status": "COMPLETE"
+        });
+        tm.on_order(&data).await;
+        let (pnl, pnl_str) = tm.get_pnl().await;
+
+        assert_eq!(pnl, 2000.0);
+        assert_eq!(pnl_str, "BUY INFY x 100 : 2000.00");
+
+        let data = json!({
+            "lp": "100.0",
+            "tk": "618"
+        });
+        tm.on_tick(&data).await;
+
+        let (pnl, pnl_str) = tm.get_pnl().await;
+        assert_eq!(pnl, 0.0);
+        assert_eq!(pnl_str, "BUY INFY x 100 : 0.00");
+
+        // wait for 5 seconds
     }
 }
